049e8c9c9bcb8469e40d45a755c7d6869bd1e5e8
Gapaivams <67814385+Gapaivams@users.noreply.github.com>
2025-08-20 21:44:02 -0300

    add: Atividades do dia 20/08/2025

---
+++ 20.08.25/main.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    /*
+    int num;
+    printf("Digite um numero: ");
+    scanf("%d", &num);
+
+    if (num > 0){
+        printf("O numero %d eh positivo\n", num);
+    }
+
+    if (num == 0){
+        printf("O numero %d eh zero\n", num);
+    }
+
+    if (num < 0){
+        printf("O numero %d eh negativo", num);
+    }
+    */
+
+
+    /*
+    int num;
+    printf("Digite um numero: ");
+    scanf("%d", &num);
+
+    if (num % 2 == 0){
+        printf("O numero %d eh par", num);
+    }
+
+    if (num % 2 == 1) {
+        printf("O numero %d eh impar", num);
+    }
+    */
+
+
+    /*
+    int num;
+    printf("Digite um numero: ");
+    scanf("%d", &num);
+
+    if (num > 0){
+        printf("O numero %d eh positivo\n", num);
+    }else{
+        printf("O numero %d eh negativo", num);
+    }
+    */
+
+
+    /*
+    int num1, num2;
+    printf("Digite um numero: ");
+    scanf("%d", &num1);
+    printf("Digite outro numero: ");
+    scanf("%d", &num2);
+
+    if (num1 == num2){
+        printf("Os numeros sao iguais");
+    } else {
+        printf("Os numeros digitados sao diferentes");
+    }
+    */
+
+
+
+    /*
+    int num1, num2, soma;
+    printf("Digite um numero: ");
+    scanf("%d", &num1);
+    printf("Digite outro numero: ");
+    scanf("%d", &num2);
+
+    soma = num1 + num2;
+
+    if (soma > 10){
+        printf("O resultado eh %d", soma);
+    } else{
+        printf("O resultado eh menor que 10");
+    }
+    */
+
+
+
+    int num1, num2, soma;
+    printf("Digite um numero: ");
+    scanf("%d", &num1);
+    printf("Digite outro numero: ");
+    scanf("%d", &num2);
+
+    if (num1 < num2){
+        printf("%d, %d", num1, num2);
+    } else{
+        printf("%d, %d", num2, num1);
+    }
+
+
+
+
+
+    return 0;
+}
